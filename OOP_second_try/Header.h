21c313823f3a0b558a31cc3df4ffd4eb47dea477
matei-c <matei.ciocanescu7@gmail.com>
2022-11-24 11:25:04 +0200

    Added and updated some getters and setters for location class. Set default values for class attributes.

--- OOP_second_try/Header.h
+++ OOP_second_try/Header.h
@@ -4,29 +4,43 @@
 
 class location {
 private:
-	char name[100]; // name of the stadium
-	int maxNoOfSeats; // total seats available in the stadium
-	int noOfRows;
-	int noOfColumns;
-	int* stands; // eg: North, South, East, West formatted as numbers eg: 1,2,3
-	char* section; // eg: lawn, tribune, VIP, press
-	int* zones; // each section has more zones counted in numbers eg: 100,101,102
+	char name[100] = "Unknown"; // name of the stadium
+	int maxNoOfSeats = 1; // total seats available in the stadium
+	int noOfRows = 1;
+	int noOfColumns = 1;
+	int* stands = nullptr; // eg: North, South, East, West formatted as numbers eg: 1,2,3
+	char* section = nullptr; // eg: lawn, tribune, VIP, press
+	int* zones = nullptr; // each section has more zones counted in numbers eg: 100,101,102
 
 	location(const char* name, int maxNoOfSeats, int noOfRows, int noOfColumns) { // constructor for location
 		this->setMaxSeats(maxNoOfSeats);
 		this->setName(name);
+		this->setRows(noOfRows);
+		this->setColumns(noOfColumns);
 	}
 
 
 public:
-	std::string getNameValue() { //getter for name
+	std::string getName() { //getter for name as string
 		return std::string(this->name);
 	}
 
-	char* getNameValueAsPointer() {
+	char* getNameAsPointer() { //getter for name as pointer
 		char* nameCopy = new char[strlen(this->name) + 1];
 		strcpy_s(nameCopy, strlen(this->name) + 1, this->name);
 		return nameCopy;
+	}
+
+	int getMaxSeats() { //getter for max no of seats
+		return this->maxNoOfSeats;
+	}
+
+	int getRows() {
+		return this->noOfRows;
+	}
+
+	int getColumns() {
+		return this->noOfColumns;
 	}
 
 private:
@@ -41,41 +55,51 @@ private:
 		if (value > 0)
 			this->maxNoOfSeats = value;
 		else
-			throw std::exception("Value can not be negative or null");
+			throw std::exception("Value of seats can not be negative or null");
 	}
 
-public:
-	int getMaxSeats() { //getter for max no of seats
-		return this->maxNoOfSeats;
+	void setRows(int value) {
+		if (value > 0)
+			this->noOfRows = value;
+		else
+			throw std::exception("Value of rows can not be negative or null");
 	}
+
+	void setColumns(int value) {
+		if (value > 0)
+			this->noOfColumns = value;
+		else
+			throw std::exception("Value of columns can not be negative or null");
+	}
+
 
 };
 
 class ticket {
 private:
-	int seat; //column of the position in the stand
-	int row;
-	int stand;
-	char* section;
-	int zone;
-	std::string date; //date format DD-MM-YY
-	std::string time; //time format 15:47
-	std::string ID; //nominal ticket, corresponds with a unique identification no.
+	int seat = 0; //column of the position in the stand
+	int row = 0;
+	int stand = 0;
+	char* section = nullptr;
+	int zone = 0;
+	std::string date = "01-01-23"; //date format DD-MM-YY
+	std::string time = "00:00"; //time format 15:47
+	std::string ID = "00000000"; //nominal ticket, corresponds with a unique identification no.
 
 	ticket(int seat, int row, int stand, int zone, char section) { //constructor
-		seat = 1;
-		row = 1;
-		stand = 1;
+		//seat = 1;
+		//row = 1;
+		//stand = 1;
 		//section = "lawn";
-		zone = 100;
+		//zone = 100;
 	}
 };
 
 class event {
 private:
-	char* date; //date format DD-MM-YY
-	char* time; //time format 15:47
-	char name[100];
+	char* date = nullptr; //date format DD-MM-YY
+	char* time = nullptr; //time format 15:47
+	char name[100] = "Unknown";
 };
 
 //general info about a specific match
